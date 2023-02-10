ea1631f37606b60254ff9138758490aea8ac1030
Maria Jordana <jordana-cavalcante@outlook.com>
2023-02-10 12:04:00 -0300

    removing contato and sobre

--- projeto/urls.py
+++ projeto/urls.py
@@ -17,11 +17,9 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
 
-from recipes.views import home, sobre ,contato
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
-
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/home.html', context={'name' : 'Jojo'})
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
