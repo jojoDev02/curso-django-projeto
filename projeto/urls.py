f17ca9d240867e3e077c92f4e788d5734141b5c4
Maria Jordana <jordana-cavalcante@outlook.com>
2023-02-23 15:13:52 -0300

    registering recipe and category models

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -23,3 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from recipes.models import Category, Recipe
+
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Recipe, RecipeAdmin )
