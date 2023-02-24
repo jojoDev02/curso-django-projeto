ed0dac16ec1b9b745fae53a636c8ef2b55e158a2
Maria Jordana <jordana-cavalcante@outlook.com>
2023-02-24 15:39:19 -0300

    add real models in view and correcting null author

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
+from recipes.models import Recipe
 
 from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published = True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id = category_id, is_published = True ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
     })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context= {
-        'recipe' : make_recipe(),
-        'is_page_details' : True,
+    recipe = Recipe.objects.filter(pk=id,is_published = True).first()
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
     })
