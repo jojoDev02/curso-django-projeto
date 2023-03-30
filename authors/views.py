33414036b9d77d9575fa7be8394ca2ac6578af0d
Maria Jordana <jordana-cavalcante@outlook.com>
2023-03-30 14:49:59 -0300

    creating form for register user author

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from authors.forms import RegisterForm
+
 def register_view(request):
-    return render(request, 'authors/register_view.html')
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+    return render(request, 'authors/register_view.html', {
+        'form': form,
+    })
